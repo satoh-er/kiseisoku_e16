cb906beb441bb3e4085249a5427bed17e4c8195f
vaio-S14-Satoh\satoh <satoh@satoh-er.com>
2022-08-28 15:47:32 +0900

    不要な変数の削除

--- src/room.py
+++ src/room.py
@@ -15,12 +15,6 @@ class BuildingPart:
     # 面積
     area: float
 
-    # 方位角[rad]
-    wa: float
-
-    # 傾斜角[rad]
-    wb: float
-
     # 温度差係数
     temp_diff_coeff: float
 
@@ -29,9 +23,6 @@ class BuildingPart:
 
     # 熱貫流率[W/(m2･K)]
     u_value: float
-
-    # 日射熱取得率[－]（透明な開口部のみ指定）
-    eta_value: float
 
     # 放射空調の発熱比率
     flr: float
@@ -47,61 +38,6 @@ class BuildingPart:
     rfa1: np.ndarray
     rft1: np.ndarray
 
-    # 傾斜面の特性値
-    wz: float = 0.0
-    ww: float = 0.0
-    ws: float = 0.0
-
-    # 入射角の方向余弦
-    cos_theta: np.ndarray = None
-
-    # 入射日射量（直達と天空）
-    i_d: np.ndarray = None
-    i_s: np.ndarray = None
-
-    def calc_wz_ww_ws(self):
-        """
-        傾斜面の特性値計算
-        :return:
-        """
-
-        self.wz = math.cos(self.wb)
-        self.ww = math.sin(self.wb) * math.sin(self.wa)
-        self.ws = math.sin(self.wb) * math.cos(self.wa)
-
-    def calc_cos_theta(self, sh: np.ndarray, sw: np.ndarray, ss: np.ndarray):
-        """
-        入射角方向余弦の計算
-        :param sh: sin(h)
-        :param sw: cos(h) * sin(a)
-        :param ss: cos(h) * cos(a)
-        :return:
-        """
-
-        self.cos_theta = np.maximum(sh * self.wz + sw * self.ww + ss * self.ws, 0.0)
-
-    def calc_slope_sol(self, idn: np.ndarray, isky: np.ndarray):
-        """
-        傾斜面日射量の計算
-        :param idn: 法線面直達日射量[W/m2]
-        :param isky: 水平面天空日射量[W/m2]
-        :return:
-        """
-
-        # 直達日射量
-        self.i_d = self.cos_theta * idn
-
-        # 天空日射量
-        self.i_s = (1.0 + math.cos(self.wb)) / 2.0 * isky
-
-    def calc_q_gt(self) -> np.ndarray:
-        """
-        透過日射熱取得の計算[W]
-        :return: 透過日射熱取得[W]
-        """
-
-        return (self.i_d + self.i_s) * self.eta_value
-
 # end::tagname[]
 
 
@@ -109,11 +45,7 @@ class Room:
 
     def __init__(self, air_volume: float,
                  vent_volume: float,
-                 beta: float,
-                 is_cooling_convenction: bool,
-                 is_heating_convvection: bool,
-                 cooling_capacity: float,
-                 heating_capacity: float):
+                 beta: float):
         """
         Roomクラスの初期化
         :param air_volume: 室気積[m3]
@@ -132,7 +64,7 @@ class Room:
         self.cap = self.air_volume * ct.rho_a * ct.c_a + self.air_volume * ct.c_frt * 1000.0
 
         # 換気量[m3/h] →[m3/s]
-        self.vent_volume = vent_volume / 3600.0
+        self.vent_volume = vent_volume
 
         # 放射暖房の対流比率β
         self.beta = beta
@@ -140,39 +72,18 @@ class Room:
         # 部位情報
         self.building_parts = []
 
-        # 冷房方式
-        self.is_cooling_convection = is_cooling_convenction
-
-        # 暖房方式
-        self.is_heating_convection = is_heating_convvection
-
-        # 冷房能力[kW]
-        self.l_c_c = 0.0
-        self.l_r_c = 0.0
-        if self.is_cooling_convection:
-            self.l_c_c = - cooling_capacity
-        else:
-            self.l_r_c = - cooling_capacity
-
-        # 暖房能力
-        self.l_c_h = 0.0
-        self.l_r_h = 0.0
-        if self.is_heating_convection:
-            self.l_c_h = heating_capacity
-        else:
-            self.l_r_h = heating_capacity
-
     def building_part_append(self,
                              name: str,
                              area: float,
-                             wa: float,
-                             wb: float,
-                             bp_type: str,
                              temp_diff_coeff: float,
                              hi: float,
                              flr: float,
-                             bp_list: list,
-                             walls: wl.Wall):
+                             u_value: float,
+                             rfa0: float,
+                             rft0: float,
+                             r: np.ndarray,
+                             rfa1: np.ndarray,
+                             rft1: np.ndarray):
         """
         建物の部位を追加登録する関数
         :param name: 部位名称
@@ -188,26 +99,19 @@ class Room:
         :return:
         """
 
-        wall = walls[bp_list.index(bp_type)]
         self.building_parts.append(BuildingPart(
             name=name,
             area=area,
-            wa=math.radians(wa),
-            wb=math.radians(wb),
             temp_diff_coeff=temp_diff_coeff,
             hi=hi,
-            u_value=wall.u_value,
-            eta_value=wall.eta_value,
+            u_value=u_value,
             flr=flr,
-            rfa0=wall.rfa0,
-            rft0=wall.rft0,
-            r=wall.r,
-            rfa1=wall.rfa1,
-            rft1=wall.rft1
+            rfa0=rfa0,
+            rft0=rft0,
+            r=r,
+            rfa1=rfa1,
+            rft1=rft1
         ))
-
-        # 追加した部位の傾斜面特性値の計算
-        self.building_parts[-1].calc_wz_ww_ws()
 
     def calc_f(self) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray):
         """
@@ -248,7 +152,7 @@ class Room:
         :return:
         """
 
-        a_js = np.array([bp.area for bp in self.building_parts])
+        a_js = np.array([bp.area for bp in self.building_parts]).flatten()
 
         fot_js = a_js / np.sum(a_js)
 
@@ -274,7 +178,7 @@ class Room:
         :return:
         """
 
-        eps = math.exp(- a0 / self.cap * ct.preheat_time)
+        eps = np.exp(- a0 / self.cap * ct.preheat_time)
 
         return eps
 
@@ -314,10 +218,13 @@ class Room:
         """
 
         # 各種numpy配列を作成
-        a_js = np.array([bp.area for bp in self.building_parts])
-        hi_js = np.array([bp.hi for bp in self.building_parts])
+        n_bndrs = len(self.building_parts)
+        a_js_ns = np.tile(np.array([bp.area for bp in self.building_parts]).reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
+        hi_js_ns = np.tile(np.array([bp.hi for bp in self.building_parts]).reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
+        f_wqss_js_ns = np.tile(f_wqss_js.reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
+        f_wscs_js_ns = np.tile(f_wscs_js.reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
 
-        b2 = np.sum(a_js * hi_js * (f_wqss_js * q_sol_s_js + f_wscs_js * theta_eo_s_js))\
+        b2 = np.sum(a_js_ns * hi_js_ns * (f_wqss_js_ns * q_sol_s_js + f_wscs_js_ns * theta_eo_s_js), axis=0)\
                   + ct.c_a * ct.rho_a * self.vent_volume * theta_o_s + H_n
 
         return b2
@@ -361,15 +268,21 @@ class Room:
         """
         係数c3の計算（毎時計算が必要）
         :param k_c: 人体表面の対流熱伝達比率
-        :param k_r: 仁田表面の放射熱伝達比率
+        :param k_r: 人体表面の放射熱伝達比率
         :param theta_eo_s_js: 部位j裏面の相当外気温度[℃]
         :param q_sol_s_js: 部位jの室内表面の透過日射熱取得[W/m2]
         :return:
         """
 
-        temp = k_c + k_r * np.sum(f_ot_js * f_wsr_js)
+        n_bndrs = len(f_ot_js)
+        f_ot_js_ns = np.tile(f_ot_js.reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
+        f_wsr_js_ns = np.tile(f_wsr_js.reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
+        f_wscs_js_ns = np.tile(f_wscs_js.reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
+        f_wqss_js_ns = np.tile(f_wqss_js.reshape(n_bndrs, 1), (1, 8760 * 4 + 1))
 
-        c3 = - k_r * np.sum(f_ot_js * (f_wscs_js * theta_eo_s_js + f_wqss_js * q_sol_s_js)) / temp
+        temp = k_c + k_r * np.sum(f_ot_js_ns * f_wsr_js_ns)
+
+        c3 = - k_r * np.sum(f_ot_js_ns * (f_wscs_js_ns * theta_eo_s_js + f_wqss_js_ns * q_sol_s_js)) / temp
 
         return c3
 
@@ -423,7 +336,8 @@ class Room:
         d2: float,
         d3: float,
         theta_ot_set: float,
-        mode: str
+        l_c: float,
+        l_r: float
         ) -> float:
         """
         最低保障温度の計算（毎時計算が必要）
@@ -431,12 +345,5 @@ class Room:
         :param mode: 空調モード（冷房or暖房）
         :return:
         """
-
-        if mode == 'C':
-            l_c = self.l_c_c
-            l_r = self.l_r_c
-        else:
-            l_c = self.l_c_h
-            l_r = self.l_r_h
 
         return d0 * theta_ot_set + d1 * l_c + d2 * l_r + d3
